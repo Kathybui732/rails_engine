88904404efa4a3dc2781bdd770b4ff64173bf909
Kathy Bui <kathy.bui1992@gmail.com>
2020-08-28 20:34:30 -0600

    Create relationship between items and invoices Use joins table invoice items to make the connection Migration Created All tests passing

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -2,4 +2,5 @@ class InvoiceItem < ApplicationRecord
   validates :quantity, :unit_price, presence: true
 
   belongs_to :invoice
+  belongs_to :item
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_29_022540) do
+ActiveRecord::Schema.define(version: 2020_08_29_023251) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,7 +28,9 @@ ActiveRecord::Schema.define(version: 2020_08_29_022540) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "invoice_id"
+    t.bigint "item_id"
     t.index ["invoice_id"], name: "index_invoice_items_on_invoice_id"
+    t.index ["item_id"], name: "index_invoice_items_on_item_id"
   end
 
   create_table "invoices", force: :cascade do |t|
@@ -66,6 +68,7 @@ ActiveRecord::Schema.define(version: 2020_08_29_022540) do
   end
 
   add_foreign_key "invoice_items", "invoices"
+  add_foreign_key "invoice_items", "items"
   add_foreign_key "invoices", "customers"
   add_foreign_key "items", "merchants"
   add_foreign_key "transactions", "invoices"
