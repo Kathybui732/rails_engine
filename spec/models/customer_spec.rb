a668ffa5ede28ca3824a28547331aa046956612e
Kathy Bui <kathy.bui1992@gmail.com>
2020-08-28 19:13:53 -0600

    Create spec for customer

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -2,16 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
   describe 'validations' do
-    it { should validate_presence_of :id }
     it { should validate_presence_of :first_name }
     it { should validate_presence_of :last_name }
-    it { should validate_presence_of :created_at }
-    it { should validate_presence_of :updated_at }
   end
 
-  # describe 'relationships' do
-  #   it { should belong_to :model }
-  #   it { should have_many :joins_table }
-  #   it { should have_many(:model).through(:joins_table) }
-  # end
+  describe 'relationships' do
+    # it { should have_many :invoices }
+  end
 end
