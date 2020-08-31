18c75d6dfab9f054f782ba3b50b38d7e077bdce6
Kathy Bui <kathy.bui1992@gmail.com>
2020-08-31 16:03:39 -0600

    Update spec to test for json respones

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -8,9 +8,13 @@ RSpec.describe 'Merchant API' do
 
     expect(response).to be_successful
 
-    merchants = JSON.parse(response.body)
+    merchants = JSON.parse(response.body, symbolize_names: true)
 
-    expect(merchants.count).to eq(3)
+    expect(merchants[:data].size).to eq(3)
+    expect(merchants[:data][0]).to have_key(:id)
+    expect(merchants[:data][0]).to have_key(:type)
+    expect(merchants[:data][0]).to have_key(:attributes)
+    expect(merchants[:data][0][:attributes]).to have_key(:name)
   end
 
   it "can find a merchant" do
@@ -18,10 +22,14 @@ RSpec.describe 'Merchant API' do
 
     get "/api/v1/merchants/#{id}"
 
-    merchant = JSON.parse(response.body)
+    merchant_json = JSON.parse(response.body, symbolize_names: true)
+    merchant = Merchant.find(id)
 
     expect(response).to be_successful
-    expect(merchant['id']).to eq(id)
+    expect(merchant_json[:data][:id]).to eq("#{id}")
+    expect(merchant_json[:data][:type]).to eq('merchant')
+    expect(merchant_json[:data][:attributes][:name]).to eq(merchant.name)
+
   end
 
   it "can create a merchant" do
@@ -29,9 +37,13 @@ RSpec.describe 'Merchant API' do
 
     post '/api/v1/merchants', params: { merchant: merchant_params }
     merchant = Merchant.last
+    merchant_json = JSON.parse(response.body, symbolize_names: true)
 
     expect(response).to be_successful
     expect(merchant.name).to eq(merchant_params[:name])
+    expect(merchant_json[:data][:id]).to eq("#{merchant.id}")
+    expect(merchant_json[:data][:type]).to eq('merchant')
+    expect(merchant_json[:data][:attributes][:name]).to eq(merchant.name)
   end
 
   it "can update a merchant" do
@@ -41,19 +53,28 @@ RSpec.describe 'Merchant API' do
 
     patch "/api/v1/merchants/#{id}", params: { merchant: merchant_params }
     merchant = Merchant.find(id)
+    merchant_json = JSON.parse(response.body, symbolize_names: true)
 
     expect(response).to be_successful
     expect(merchant.name).to_not eq(previous_name)
     expect(merchant.name).to eq(merchant_params[:name])
+    expect(merchant_json[:data][:id]).to eq("#{merchant.id}")
+    expect(merchant_json[:data][:type]).to eq('merchant')
+    expect(merchant_json[:data][:attributes][:name]).to eq(merchant_params[:name])
   end
 
   it "can destroy a merchant" do
-    merchant_id = create(:merchant).id
+    merchant = create(:merchant)
 
     expect(Merchant.count).to eq(1)
-    expect{ delete "/api/v1/merchants/#{merchant_id}" }.to change(Merchant, :count).by(-1)
+    expect{ delete "/api/v1/merchants/#{merchant.id}" }.to change(Merchant, :count).by(-1)
+    merchant_json = JSON.parse(response.body, symbolize_names: true)
+
     expect(response).to be_successful
+    expect(merchant_json[:data][:id]).to eq("#{merchant.id}")
+    expect(merchant_json[:data][:type]).to eq('merchant')
+    expect(merchant_json[:data][:attributes][:name]).to eq(merchant.name)
     expect(Merchant.count).to eq(0)
-    expect{Merchant.find(merchant_id)}.to raise_error(ActiveRecord::RecordNotFound)
+    expect{Merchant.find(merchant.id)}.to raise_error(ActiveRecord::RecordNotFound)
   end
 end
