7c5e1385b71e4a39cf0d46cd131b5430672342d1
Kathy Bui <kathy.bui1992@gmail.com>
2020-09-01 16:17:32 -0600

    Create spec and update controller to filter by updated_at

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -14,6 +14,7 @@ class Api::V1::Items::SearchController < ApplicationController
     results = results.filter_by_unit_price(params[:unit_price]) if params[:unit_price].present?
     results = results.filter_by_merchant_id(params[:merchant_id]) if params[:merchant_id].present?
     results = results.filter_by_created_at(params[:created_at]) if params[:created_at].present?
+    results = results.filter_by_updated_at(params[:updated_at]) if params[:updated_at].present?
     results
   end
 end
