e7c0c28573957cb5527051cbcd8b46c68fea89b8
Kathy Bui <kathy.bui1992@gmail.com>
2020-09-01 15:40:25 -0600

    Fix models to use dependents: destroy_all to fix rake task

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,9 +1,9 @@
 class Invoice < ApplicationRecord
   validates :status, presence: true
 
-  belongs_to :customer, dependent: :destroy
+  belongs_to :customer
   has_many :transactions, dependent: :destroy
   has_many :invoice_items, dependent: :destroy
   has_many :items, through: :invoice_items
-  belongs_to :merchant, dependent: :destroy
+  belongs_to :merchant
 end
