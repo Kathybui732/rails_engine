44fd9c0ddff67e6bd7111ba0a885bae69e711067
Kathy Bui <kathy.bui1992@gmail.com>
2020-08-31 19:35:58 -0600

    Create relationships for items and merchants Create spec to test that GET /api/v1/merchants/:id/items renders a json with all items belonging to merchant Create spec to test that GET /api/v1/items/:id/merchant return the merchant associated with an item Nested controllers created for both resources Routes updated to reflect routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,21 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :items, only: [:index, :show, :create, :update, :destroy]
-      resources :merchants, only: [:index, :show, :create, :update, :destroy]
+      resources :items, only: [:index, :show, :create, :update, :destroy] do
+        get '/merchant', to: 'items/merchant#index'
+      end
+
+      resources :merchants, only: [:index, :show, :create, :update, :destroy] do
+        get '/items', to: 'merchants/items#index'
+      end
     end
   end
 
   # namespace :api do
   #   namespace :v1 do
   #     namespace :merchants do
-  #       get '/find', to: 'search#'
+  #       resources :items, only: [:index]
+  #       # get '/find', to: 'search#'
   #     end
   #   end
   # end
