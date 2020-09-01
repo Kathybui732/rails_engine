f6a04970a6a22324a9d84c78a9cde1103af2b5fc
Kathy Bui <kathy.bui1992@gmail.com>
2020-09-01 16:15:06 -0600

    Create spec and update controller to filter by merchant_id

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -10,6 +10,9 @@ class Api::V1::Items::SearchController < ApplicationController
     results = Item.all
     results = results.filter_by_name(params[:name]) if params[:name].present?
     results = results.filter_by_id(params[:id]) if params[:id].present?
+    # results = results.filter_by_description(params[:description]) if params[:description].present?
+    results = results.filter_by_unit_price(params[:unit_price]) if params[:unit_price].present?
+    results = results.filter_by_merchant_id(params[:merchant_id]) if params[:merchant_id].present?
     results
   end
 end
