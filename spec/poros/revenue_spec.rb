3673b5e6845837426a00eecf7de67a5fb7a798b3
Kathy Bui <kathy.bui1992@gmail.com>
2020-09-02 21:08:07 -0600

    Update test for poro

--- spec/poros/revenue_spec.rb
+++ spec/poros/revenue_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe 'Revenue' do
+describe 'Revenue' do
   it "is initialize with a revenue" do
-    revenue = Revenue.new(10)
-    expect(revenue.revenue).to eq(10)
+    rev = Revenue.new(10)
+    expect(rev.revenue).to eq(10)
   end
 end
