9f54b75ad5baa1d95aa5e883c85d387de760d65c
Kathy Bui <kathy.bui1992@gmail.com>
2020-09-02 19:53:09 -0600

    Create endpoint to find a variable number of merchants selling the most items Spec created to test request/response is working properly configure routes to include api/v1/merchants/most_/items that route to a most items controller Update model to include method to return merchants with most items sold Created spec to test most_items model method Controller uses Merchant serializer and model methods to get a list of merchants to pass to the json to render.

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe Merchant, type: :model do
     it { should have_many(:transactions).through(:invoices) }
   end
 
-  describe 'methods' do
+  describe 'most_revenue  ' do
     it "#revenue" do
       m1, m2, m3 = create_list(:merchant, 3)
       it1 = create(:item, merchant_id: m1.id)
@@ -28,6 +28,24 @@ RSpec.describe Merchant, type: :model do
       t3 = create(:transaction, invoice_id: in3.id, result: 'success', created_at: '2012-03-26 14:53:59 UTC' )
 
       expect(Merchant.most_revenue(1)).to eq([m3])
+    end
+
+    it "#most_items" do
+      m1, m2, m3 = create_list(:merchant, 3)
+      it1 = create(:item, merchant_id: m1.id)
+      it2 = create(:item, merchant_id: m2.id)
+      it3 = create(:item, merchant_id: m3.id)
+      in1 = create(:invoice, merchant_id: m1.id, status: 'shipped')
+      in2 = create(:invoice, merchant_id: m2.id, status: 'shipped')
+      in3 = create(:invoice, merchant_id: m3.id, status: 'shipped')
+      ii1 = create(:invoice_item, invoice_id: in1.id, item_id: it1.id, quantity: 1, unit_price: 10 )
+      ii2 = create(:invoice_item, invoice_id: in2.id, item_id: it2.id, quantity: 2, unit_price: 20 )
+      ii3 = create(:invoice_item, invoice_id: in3.id, item_id: it3.id, quantity: 3, unit_price: 30 )
+      t1 = create(:transaction, invoice_id: in1.id, result: 'success', created_at: '2012-03-25 14:53:59 UTC' )
+      t2 = create(:transaction, invoice_id: in2.id, result: 'success', created_at: '2012-03-26 14:53:59 UTC' )
+      t3 = create(:transaction, invoice_id: in3.id, result: 'success', created_at: '2012-03-26 14:53:59 UTC' )
+
+      expect(Merchant.most_items(1)).to eq([m3])
     end
   end
 end
